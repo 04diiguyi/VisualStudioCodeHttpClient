d4c01f05a6be9ea39ef6860e0cb5c44dfa6b3459
Yi Gu <guyi@microsoft.com>
2020-09-30 16:43:44 -0700

    Sample code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # VisualStudioCodeHttpClient
+
+This is the sample code of using Visual Studio Code Http Client https://marketplace.visualstudio.com/items?itemName=humao.rest-client.
+
+
+In this project, there are two components. The first is the TestService which is Http Request Triggered Azure Function and has three endpoints. HttpClient is the VS code to send and receive messages from TestService.
---
+++ TestService/TestService/Function1.cs
@@ -0,0 +1,76 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace TestService
+{
+    public class Function
+    {
+        private static Dictionary<Guid, User> users = new Dictionary<Guid, User>();
+
+        [FunctionName("GetUser")]
+        public IActionResult GetUser(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "user")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string idInString = req.Query["id"];
+
+            var result = Guid.TryParse(idInString, out Guid id);
+
+            if (!result)
+            {
+                return new BadRequestObjectResult("Name is empty");
+            }
+            else
+            {
+                users.TryGetValue(id, out User user);
+
+                return new OkObjectResult(user);
+            }
+        }
+
+        [FunctionName("GetUserByRoute")]
+        public IActionResult GetUserByRoute(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "user/{id}")] HttpRequest req,
+            Guid id,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            users.TryGetValue(id, out User user);
+
+            return new OkObjectResult(user);
+        }
+
+        [FunctionName("AddUser")]
+        public async Task<IActionResult> AddUser(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "user")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var user = JsonSerializer.Deserialize<User>(requestBody);
+
+            var id = Guid.NewGuid();
+            users.Add(id, user);
+
+            return new OkObjectResult(id);
+        }
+    }
+
+    public class User
+    {
+        public string FirstName{ set; get; }
+        public string LastName { set; get; }
+    }
+}
